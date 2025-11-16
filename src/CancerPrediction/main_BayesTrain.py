1c4ad090a49a2407343012608c7b2b0fcb5eb7cf
levantuananh <tuananhlevan181@gmail.com>
2025-11-16 12:16:18 +0700

    refactor

--- src/CancerPrediction/main_BayesTrain.py
+++ src/CancerPrediction/main_BayesTrain.py
@@ -4,7 +4,6 @@ from torch.nn import functional as F
 
 from BayesModel import CancerPredictionNet
 from BayesTrain import Bayes_train
-from CNNTrain import CNN_train
 
 torch.manual_seed(42)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
